6dc6e1348f57e786d3327763d5be5b994a746151
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-06 10:59:36 -0300

    chore: absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     test: { globals: true, environment: "jsdom", setupFiles: "./test.setup.js" },
+    resolve: {
+        alias: [{ find: "@", replacement: "/src" }],
+    },
 });
