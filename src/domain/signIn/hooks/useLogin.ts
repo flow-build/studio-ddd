e4f71b223e23a806b230080680b08714401880c9
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-06 13:01:29 -0300

    chore: useLogin handle with success

--- src/domain/signIn/hooks/useLogin.ts
+++ src/domain/signIn/hooks/useLogin.ts
@@ -1,19 +1,32 @@
 import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
 
-import useApi from "../../../core/api/useApi";
+import useApi from "~/core/api/useApi";
+import { setStorageItem } from "~/core/utils/storage";
 
 const useLogin = () => {
+    const navigate = useNavigate();
     const api = useApi();
 
-    return useMutation<string>(async () => {
-        try {
-            const response = await api.post("/token", {});
-            return response.data.jwtToken || response.data.token;
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (e: any) {
-            throw new Error(`getWorkflowAnonymousToken -> ${e.error}: ${e.message}`);
+    return useMutation<string>(
+        async () => {
+            try {
+                const response = await api.post("/token", {});
+                return response.data.jwtToken || response.data.token;
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            } catch (e: any) {
+                throw new Error(`getWorkflowAnonymousToken -> ${e.error}: ${e.message}`);
+            }
+        },
+        {
+            onSuccess: (token) => {
+                if (token) {
+                    setStorageItem("TOKEN", token);
+                    navigate("/dashboard");
+                }
+            }
         }
-    });
+    );
 };
 
 export default useLogin;
