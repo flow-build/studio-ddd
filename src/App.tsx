a5cfab4a1a635cb1e3ae692023e9d7324f3df708
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-06 10:45:27 -0300

    chore: comments removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { routes as ExampleRoutes } from "./domain/example";
-import { routes as SignInRoutes } from "./domain/signIn";
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./store";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
+import { routes as SignInRoutes } from "./domain/signIn";
+
+import { store } from "./store";
+
+import GlobalStyle from "./core/theme/globalStyle";
+import { darkTheme } from "./core/theme/darkTheme";
 
 const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
 const queryClient = new QueryClient({
@@ -23,12 +29,17 @@ function App() {
     return (
         <ReduxProvider store={store}>
             <QueryClientProvider client={queryClient}>
-                <Suspense fallback={<div>loading...</div>}>
-                    <Routes>
-                        {/* <Route path="/*" element={<ExampleRoutes />} /> */}
-                        <Route path="/*" element={<SignInRoutes />} />
-                    </Routes>
-                </Suspense>
+                <ThemeProvider theme={darkTheme}>
+                    <GlobalStyle />
+
+                    <Suspense fallback={<div>loading...</div>}>
+                        <Routes>
+                            <Route path="/*" element={<SignInRoutes />} />
+                        </Routes>
+                    </Suspense>
+                </ThemeProvider>
+
+                <ReactQueryDevtools />
             </QueryClientProvider>
         </ReduxProvider>
     );
