9e38143169a4adfea4749ed728e00cadfbf37800
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-06 12:59:39 -0300

    fix: change alias from @ to ~

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     test: { globals: true, environment: "jsdom", setupFiles: "./test.setup.js" },
     resolve: {
-        alias: [{ find: "@", replacement: "/src" }],
+        alias: [{ find: "~", replacement: "/src" }],
     },
 });
