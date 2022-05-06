a3236499fe55f353a3b086cf58ec2ab5153029d1
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-06 10:41:49 -0300

    chore: hook to handle api created

---
+++ src/core/api/useApi.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+import { getStorageItem } from "../utils/storage";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+});
+
+api.interceptors.request.use(
+    async (config) => {
+        const token = getStorageItem("TOKEN");
+
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        console.error(`Interceptors Request -> ${error.name}: ${error.message}`);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    }
+);
+
+const useApi = () => {
+    return api;
+};
+
+export default useApi;
