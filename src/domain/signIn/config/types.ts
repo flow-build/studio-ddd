c4e6b04937575d1473dbef27a231b43c08bb5b80
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-09 11:31:21 -0300

    refactor: arquivo de types não usado removido

--- src/domain/signIn/config/types.ts
+++
@@ -1,3 +0,0 @@
-export type SignIn = {
-    id: number;
-}
--- src/domain/signIn/index.ts
+++ src/domain/signIn/index.ts
@@ -1,2 +1,1 @@
 export { default as routes } from "~/domain/signIn/config/routes";
-export * as types from "~/domain/signIn/config/types";
