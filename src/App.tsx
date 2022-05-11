d9c55698e37cf92a4401a1807c54c1114a0fbe0a
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-11 17:07:24 -0300

    chore: rota para dashboard adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider as ReduxProvider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import {Global} from '@emotion/react'
+import { Global } from '@emotion/react'
 
 import { ThemeProvider } from "@mui/material/styles";
 
+import { Dashboard } from "~/core/components/Dashboard";
 
 import { queryClient } from "~/core/config/queryClient";
 import { darkTheme } from "~/core/theme/darkTheme";
 import globalStyle from "~/core/theme/globalStyle";
 
 import { routes as SignInRoutes } from "~/domain/signIn";
+import { routes as WorkflowsRoutes } from "~/domain/workflows";
 
 import { store } from "~/store";
 
@@ -26,7 +28,10 @@ function App() {
 
                     <Suspense fallback={<div>loading...</div>}>
                         <Routes>
-                            <Route path="/*" element={<SignInRoutes />} />
+                            <Route path="/" element={<SignInRoutes />} />
+                            <Route path="dashboard" element={<Dashboard />}>
+                                <Route path="workflows" element={<WorkflowsRoutes />} />
+                            </Route>
                         </Routes>
                     </Suspense>
                 </ThemeProvider>
