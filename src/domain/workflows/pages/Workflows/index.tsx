f85f47113c3e305ddfb6d5ca06a16464aeaa30cc
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-11 17:05:39 -0300

    chore: tela de workflows criado

--- src/domain/workflows/pages/Workflows/index.tsx
+++ src/domain/workflows/pages/Workflows/index.tsx
@@ -1,23 +1,55 @@
-import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
-import Button from "../../components/Button";
-import { setCounter } from "../../ducks/workflows";
-import { RootState } from "../../ducks";
+import { useEffect, useState } from 'react';
+
+import _isEqual from 'lodash/isEqual'
+
+import { ContentHeader } from '~/core/components/ContentHeader'
+import { ModeView } from '~/core/constants/ModeView';
+import { IWorkflow } from '~/core/models/IWorkflow';
+
+import { CardsView } from '~/domain/workflows/components/CardsView'
+import { useTable, useWorkflow } from '~/domain/workflows/hooks'
+
+import * as S from './styles';
 
 const Workflows = () => {
-    const dispatch = useDispatch();
-    const counter = useSelector((store: RootState) => store.workflows.counter);
-    const [stateCounter, setStateCounter] = useState(counter);
+    const [workflows, setWorkflows] = useState<IWorkflow[]>([]);
+    const [modeView, setModeView] = useState(ModeView.LIST)
+    const [filter, setFilter] = useState('')
+
+    const { data } = useWorkflow({ search: filter })
+    const table = useTable(workflows)
+
+    const onChangeModeView = (newModeView: ModeView) => {
+        setModeView(newModeView)
+    }
+
+    const onFilter = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setFilter(event.target.value)
+    }
+
+    useEffect(() => {
+        setWorkflows(data?.reverse() ?? [])
+    }, [data])
 
     return (
-        <div>
-            <h1>Workflows</h1>
-            <h2>State counter - {stateCounter}</h2>
-            <h2>Redux counter - {counter}</h2>
-            <Button onClick={() => setStateCounter(actualValue => actualValue + 1)}>ADD +</Button>
-            <Button onClick={() => dispatch(setCounter(stateCounter))}>Push to redux</Button>
-        </div>
-    );
+        <S.Wrapper>
+            <ContentHeader
+                title='Workflows'
+                inputLabel="Nome / ID"
+                onChangeModeView={onChangeModeView}
+                onChangeInput={onFilter}
+            />
+
+            {_isEqual(modeView, ModeView.CARDS) && <CardsView workflows={workflows ?? []} />}
+
+            {_isEqual(modeView, ModeView.LIST) && (
+                <S.TableContainer>
+                    <S.Table columnData={table.columnData} rows={table.rows} />
+                </S.TableContainer>
+            )}
+
+        </S.Wrapper>
+    )
 };
 
 export default Workflows;
