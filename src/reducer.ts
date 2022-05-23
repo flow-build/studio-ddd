fe320685353b43e17953185e6f7bfa7c3ae58235
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-23 16:05:57 -0300

    chore: reducer do histórico adicionado

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
+import { reducers as historyReducers, RootState as HistoryRootState } from "~/domain/history/ducks";
 import { reducers as signInReducers, RootState as SignInRootState } from "~/domain/signIn/ducks";
 import { reducers as workflowsReducers, RootState as WorkflowsRootState } from "~/domain/workflows/ducks";
 
-export type RootState = SignInRootState & WorkflowsRootState;
+export type RootState = SignInRootState & WorkflowsRootState & HistoryRootState;
 
 export default combineReducers({
     ...signInReducers,
     ...workflowsReducers,
+    ...historyReducers
 });
