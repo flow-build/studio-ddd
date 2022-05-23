9ad7566e92fb23425c17b893a204827ca012438f
elsonfojr-fdte <elson.oliveira@fdte.io>
2022-05-23 14:34:50 -0300

    chore: adicionado reducer do processo

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
+import { reducers as processesReducers, RootState as ProcessesRootState } from "~/domain/processes/ducks";
 import { reducers as signInReducers, RootState as SignInRootState } from "~/domain/signIn/ducks";
 import { reducers as workflowsReducers, RootState as WorkflowsRootState } from "~/domain/workflows/ducks";
 
-export type RootState = SignInRootState & WorkflowsRootState;
+export type RootState = SignInRootState & WorkflowsRootState & ProcessesRootState;
 
 export default combineReducers({
     ...signInReducers,
     ...workflowsReducers,
+    ...processesReducers
 });
